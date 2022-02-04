fb3cd92f415f5b3da6a24954b70923f936f7f447
Kobe McManus <kobe.mcmanus@xenixi.com>
2022-02-04 02:18:30 -0500

    renamed

--- src/apatloc/main.java
+++ src/apatloc/main.java
@@ -8,8 +8,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -17,6 +15,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
+		
 		Scanner input = new Scanner(System.in);
 		try {
 			while (true) {
